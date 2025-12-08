7baa8cfe9200e977eacd6ef5827556953779e4b5
soushi888 <sacha.pignot@protonmail.com>
2025-12-08 18:21:38 -0500

    bump: update requests-and-offers to v0.2.2

--- Casks/requests-and-offers.rb
+++ Casks/requests-and-offers.rb
@@ -1,12 +1,12 @@
 cask "requests-and-offers" do
-  # v0.2.1 - Infrastructure & Debugging Enhancement Release
-  version "0.2.1"
+  # v0.2.2 - Holochain v0.6 Migration & Build Infrastructure Release
+  version "0.2.2"
 
   if Hardware::CPU.arm?
-    sha256 "ab118fdff5d02c3f9c08d1e82d85ccd9dead7d8d24b8c5d803f1dc47874072be"
+    sha256 "9b5cde08c16b87c64421205f37b30c7b06a271bff113e84c6ca04d51b71ce50b"
     url "https://github.com/happenings-community/requests-and-offers-kangaroo-electron/releases/download/v#{version}/requests-and-offers.happenings-community.kangaroo-electron-#{version}-arm64.dmg"
   else
-    sha256 "b8686aaf6bdf8063b43b3efe5a87dc679925e9bd6f58abea17dd7314551e15eb"
+    sha256 "030c000e6dc45e91dcf1b799bf321b32d994336146c7a5508ac9cd73f7840067"
     url "https://github.com/happenings-community/requests-and-offers-kangaroo-electron/releases/download/v#{version}/requests-and-offers.happenings-community.kangaroo-electron-#{version}-x64.dmg"
   end
 
